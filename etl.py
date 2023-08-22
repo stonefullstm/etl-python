6e0e11452e4dcfc40c48f68238ae6176eb528a70
stonefull <stonefull.stm@gmail.com>
2023-08-22 15:18:00 -0300

    feat: Criação do módulo Transform

--- etl.py
+++ etl.py
@@ -10,4 +10,14 @@ df = pd.read_csv('fornecedores.csv')
 fornecedores = tuple(df['id_fornecedor'].tolist())
 query = f'SELECT * FROM produtos WHERE fornecedor in {fornecedores}'
 res = cur.execute(query)
-print(res.fetchall())
+produtos = res.fetchall()
+
+# Transform
+# Gera um pedido para cada fornecedor, suficiente para 3 meses,  baseado no estoque, média de vendas e prazo de entrega de cada produto. Produtos com estoque suficiente para 3 meses não serão pedidos
+pedidos = [ (
+        produto[7],
+        produto[0],
+        produto[1],
+        produto[5] * 3 - produto[4] + produto[5] * produto[6] // 30
+    ) for produto in produtos if produto[4] < produto[5] * 3]
+
