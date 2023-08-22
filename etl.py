63c6d293dda2cbdb5ca124026fa8ab40f334e23c
stonefull <stonefull.stm@gmail.com>
2023-08-22 14:39:48 -0300

    feat: Criação do arquivo fornecedores.csv e alteração da aplicação para buscar os produtos relacionados a esses fornecedores

--- etl.py
+++ etl.py
@@ -1,11 +1,13 @@
 import sqlite3
+import pandas as pd
 
 # Extract
+# Busca do banco de dados os produtos dos fornecedores relacionados em fornecedores.csv
 con = sqlite3.connect('produtos.sqlite3')
 cur = con.cursor()
 
-print('Gerar pedido para um fornecedor')
-fornecedor = int(input('Informe o fornecedor: '))
-query = f'SELECT * FROM produtos WHERE fornecedor = {fornecedor}'
+df = pd.read_csv('fornecedores.csv')
+fornecedores = tuple(df['id_fornecedor'].tolist())
+query = f'SELECT * FROM produtos WHERE fornecedor in {fornecedores}'
 res = cur.execute(query)
 print(res.fetchall())
